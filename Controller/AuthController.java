cbfc23562788a91b6da687329f4c7db6cc49b085
kimwonjun1 <monkeydmw@naver.com>
2024-04-18 15:44:55 +0900

    Add files via upload

---
+++ Controller/AuthController.java
@@ -0,0 +1,47 @@
+package com.capstone.finance.Controller;
+
+import com.capstone.finance.DTO.Member.*;
+import com.capstone.finance.DTO.Token.TokenReqDto;
+import com.capstone.finance.DTO.Token.TokenResDto;
+import com.capstone.finance.Service.AuthService;
+import com.capstone.finance.Service.MemberService;
+import com.capstone.finance.Service.VerificationTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public  class AuthController {
+    private final AuthService authService;
+    private final MemberService memberService;
+    private final VerificationTokenService verificationTokenService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto memberRequestDto) {
+
+        return ResponseEntity.ok(authService.signup(memberRequestDto));
+    }
+
+    @GetMapping("/checknickname")
+    public boolean checkNickname(@RequestParam String nickname) {
+        return memberService.checkNickname(nickname);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResDto> login(@RequestBody LoginDto loginDto) {
+        return ResponseEntity.ok(authService.login(loginDto));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@RequestBody TokenReqDto requestDto) {
+        authService.logout(requestDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenResDto> reissue(@RequestBody TokenReqDto tokenReqDto) {
+        return ResponseEntity.ok(authService.reissue(tokenReqDto));
+    }
+}
